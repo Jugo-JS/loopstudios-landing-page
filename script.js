2dfac4d4e897d8095f0933b2268c20df92e21a0d
Jugo-JS <58330823+Jugo-JS@users.noreply.github.com>
2021-07-22 14:44:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.hamburger')
+const mobileMenu = document.querySelector('.mobile-menu')
+const closeMenu = document.querySelector('.close-menu')
+
+hamburger.addEventListener('click', function() {
+    mobileMenu.style.transform = 'translateX(0)'
+    closeMenu.addEventListener('click', function() {
+        mobileMenu.style.transform = 'translateX(-100%)'
+    })
+})
