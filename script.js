fb64ea5f57ed7a7d459818c7663893260eccc003
Jugo-JS <jugoslavsubotic@gmail.com>
2021-07-23 13:23:26 +0200

    repairing close menu button

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const closeMenu = document.querySelector('.close-menu')
 
 hamburger.addEventListener('click', function() {
     mobileMenu.style.transform = 'translateX(0)'
-    closeMenu.addEventListener('click', function() {
+})
+
+closeMenu.addEventListener('click', function() {
+        console.log('click')
         mobileMenu.style.transform = 'translateX(-100%)'
     })
-})
